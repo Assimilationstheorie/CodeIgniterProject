4f16989a85061a5404c07ed7badcbd2947be9025
Konstantin Schmidt <konstantin@leafhouse.de>
2017-12-06 11:40:53 +0100

    typo translation rus

--- application/language/russian/header_lang.php
+++ application/language/russian/header_lang.php
@@ -1,6 +1,6 @@
 <?php
   $lang['html_lang'] = 'ru-RU';
-  $lang['site_title'] = 'RUSSIA';
+  $lang['site_title'] = 'Pусский';
   $lang['language_english'] = 'Aнглийский';
   $lang['language_german'] = 'Hемецкий';
   $lang['language_russian'] = 'Pусский';
