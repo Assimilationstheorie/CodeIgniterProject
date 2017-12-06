04b57330132dc2c900f83a18be5b4ad796cdb349
Konstantin Schmidt <konstantin@leafhouse.de>
2017-12-06 11:40:26 +0100

    redirect for translation

--- application/controllers/Languagecontroller.php
+++ application/controllers/Languagecontroller.php
@@ -4,7 +4,8 @@ class Languagecontroller extends CI_Controller{
   public function setLanguage() {
     $this->load->helper('url');
     $language = $this->input->post('language');
-    $this->ll->setLanguage(strtolower($language));
+    $this->ll->setLanguage(strtolower(trim($language)));
+    redirect('/', 'refresh');
   }
 
 }
