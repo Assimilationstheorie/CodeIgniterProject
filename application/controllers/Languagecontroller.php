1b42b2c4e35b421ac44d6bdc773f4c4c4134e49f
Konstantin Schmidt <konstantin@leafhouse.de>
2017-12-08 21:21:57 +0100

    add first translation changer

--- application/controllers/Languagecontroller.php
+++ application/controllers/Languagecontroller.php
@@ -3,8 +3,13 @@ class Languagecontroller extends CI_Controller{
 
   public function setLanguage() {
     $language = $this->input->post('language');
+    $current_link = $this->input->post('current_link');
     $this->ll->setLanguage(strtolower(trim($language)));
-    redirect('/', 'refresh');
+    if($current_link != 'welcome') {
+      redirect($current_link, 'refresh');
+    } else {
+      redirect('/', 'refresh');
+    }
   }
 
 }
